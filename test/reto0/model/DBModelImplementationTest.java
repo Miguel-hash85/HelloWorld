0e7309584fa5a6714572909c06bff52e4bf63afa
34606 <34606@DESKTOP-M3FVQDL>
2021-09-27 13:41:52 +0200

    Javadoc/test

--- test/reto0/model/DBModelImplementationTest.java
+++ test/reto0/model/DBModelImplementationTest.java
@@ -5,12 +5,7 @@
  */
 package reto0.model;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -20,36 +15,9 @@ public class DBModelImplementationTest {
     
     public DBModelImplementationTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    /**
-     * Test of getGreeting method, of class DBModelImplementation.
-     * @throws java.lang.Exception
-     */
     @Test
-    public void testGetGreeting() throws Exception {
-        System.out.println("getGreeting");
-        DBModelImplementation instance = new DBModelImplementation();
-        String expResult = "";
-        String result = instance.getGreeting();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testSomeMethod() {
     }
     
 }
